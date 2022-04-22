5efbc6bdbcf44c1d48c129d8290e2fe3f80f0978
Moe Alshidi <moealshidi@gmail.com>
2022-04-22 15:52:06 +0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'view/screens/login_screen.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    const MaterialApp(
+      title: "Thises",
+      debugShowCheckedModeBanner: false,
+      home: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const LoginScreen();
+  }
+}
---
+++ lib/shared/cubit/login_cubit.dart
@@ -0,0 +1,11 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+part 'login_state.dart';
+
+class LoginCubit extends Cubit<LoginState> {
+  LoginCubit() : super(LoginInitial());
+  static LoginCubit get(BuildContext context) {
+    return BlocProvider.of((context));
+  }
+}
